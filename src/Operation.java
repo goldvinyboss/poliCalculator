e374e702b9b290596e0641ee883d66b02a90a780
Vini <vinicius.lima.silva25@hotmail.com>
2026-08-17 15:55:48 -0300

    Terceira

--- src/Operation.java
+++ src/Operation.java
@@ -1,60 +1,73 @@
+import java.util.Scanner;
 abstract class Operation
 {
-    public double x;
-    public double y;
 
-    public void calculate (double x , double y )
-    {
-        this.x=x;
-        this.y=y;
-    }
+    abstract double Calculator (double x, double y);
 }
 
 class Sum extends Operation
 {
-    double sum;
-    public void calculate (double x , double y )
+    double  Calculator (double x , double y )
     {
-        this.x=x;
-        this.y=y;
-        sum = x+y;
+       return x + y;
     }
 }
 
 class Sub extends Operation
 {
-    double sub;
-    public void calculate (double x , double y )
+
+    double  Calculator (double x , double y )
     {
-        this.x=x;
-        this.y=y;
-        sub = x-y;
+        return x - y;
     }
 }
 
 class Multi extends Operation
 {
-    double multi;
-    public void calculate (double x , double y )
+    double  Calculator (double x , double y )
     {
-        this.x=x;
-        this.y=y;
-        multi = x*y;
+        return x * y;
     }
 }
 
 class Div extends Operation
 {
-    double div;
-    public void calculate (double x , double y )
+
+    double  Calculator (double x , double y )
     {
-        this.x=x;
-        this.y=y;
-        div = x/y;
+        if ( y== 0)
+        {
+            System.out.println("Error: Division by zero!");
+        }
+
+        return x/y;
     }
 }
 
 class Calculator
 {
+  static void main()
+  {
+     Scanner sc = new Scanner (System.in);
 
+       System.out.println("Sum: Enter two number");
+       Sum sum = new Sum();
+       double sumResult = sum.Calculator(sc.nextDouble(),sc.nextDouble());
+       System.out.println("Result: " + sumResult);
+
+      System.out.println("Subtraction: Enter two number");
+      Sub sub = new Sub();
+      double subResult = sub.Calculator(sc.nextDouble(),sc.nextDouble());
+      System.out.println("Result: " + subResult);
+
+      System.out.println("Multiplication: Enter two number");
+      Multi multi = new Multi();
+      double multResult = multi.Calculator(sc.nextDouble(),sc.nextDouble());
+      System.out.println("Result: " + multResult);
+
+      System.out.println("Division Enter two number");
+      Div div = new Div();
+      double divResult = div.Calculator(sc.nextDouble(),sc.nextDouble());
+      System.out.println("Result: " + divResult);
+  }
 }
